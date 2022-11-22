fcf40dd1cf4928efc667715e5018572543ae01aa
Amy <z851772198@gmail.com>
2022-11-22 23:49:38 +0800

    AspNetCore6WebApiTest

---
+++ AspNetCore6WebApiTest/AspNetCore6WebApiTest/Controllers/WeatherForecastController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore6WebApiTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet(Name = "Get2")]
+        public IActionResult Get2(string name)
+        {
+            return Ok("hello" + name);
+        }
+
+        [HttpGet(Name = "Get3")]
+        public string Get3(string name)
+        {
+            return "hello" + name;
+        }
+    }
+}
